5f7cbb2d3daa30f376988b8d643e9ee957ae6ef1
ahmedyartanveer <ahmedyartanveer@gmail.com>
2026-01-14 11:29:58 +0600

    feat: Move scheduler import and start logic to wsgi.py; remove from urls.py for cleaner structure

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from . import scheduler 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,11 +15,3 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
-
-# -----------------------------------------------
-# Scheduler Start Code (Place it here at the end)
-# -----------------------------------------------
-try:
-    scheduler.start()
-except Exception as e:
-    print(f"Scheduler failed to start: {e}")
